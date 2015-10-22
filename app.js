033c783be85d177a4f4c182cfbb8cbbc79781e87
Sameer Ismail <sam@samism.io>
2015-10-22 00:56:21 -0500

    Drastic updates

--- app.js
+++ app.js
@@ -1,31 +1,22 @@
 var express = require('express');
-var hbs = require('hbs');
-var fs = require('fs');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var spots = require('./routes/spots');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/spots', spots);
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
 
 
 // catch 404 and forward to error handler
@@ -40,28 +31,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
-
-hbs.registerPartials(__dirname + '/views/partials');
-hbs.registerPartial('nav', fs.readFileSync(__dirname + '/views/partials/nav.hbs', 'utf8'));
-hbs.registerPartial('footer', fs.readFileSync(__dirname + '/views/partials/footer.hbs', 'utf8'));
-hbs.registerPartial('head', fs.readFileSync(__dirname + '/views/partials/head.hbs', 'utf8'));
 
 module.exports = app;
